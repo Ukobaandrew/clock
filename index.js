cc0a9076d7755101e856d86a409f55ff5873b84a
Ukoba Andrew <101108667+Ukobaandrew@users.noreply.github.com>
2022-06-27 11:06:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-let btn = document.querySelector(".btn");
-let display = document.querySelector("#display");
-btn.onclick = function () {
-  display(btn.value);
-};
+setInterval(setClock, 1000);
+
+let seconds = document.querySelector(".seconds");
+let minutes = document.querySelector(".minutes");
+let hours = document.querySelector(".hours");
+
+function setClock() {
+  let currentDate = new Date();
+  let secondsratio = currentDate.getSeconds() / 60;
+  let minutesratio = (secondsratio + currentDate.getMinutes()) / 60;
+  let hoursratio = (minutesratio + currentDate.getHours()) / 12;
+
+  setRotation(seconds, secondsratio);
+
+  setRotation(minutes, minutesratio);
+
+  setRotation(hours, hoursratio);
+}
+
+function setRotation(element, rotationratio) {
+  element.style.setProperty("--rotation", rotationratio * 360);
+}
+setClock();
